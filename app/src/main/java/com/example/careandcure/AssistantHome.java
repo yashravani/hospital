4d3c1ad11ca0e709d5305452c06e934726399774
GAUTAM <dyfashionworld27@gmail.com>
2023-03-24 10:04:01 +0530

    This is project

--- app/src/main/java/com/example/careandcure/AssistantHome.java
+++ app/src/main/java/com/example/careandcure/AssistantHome.java
@@ -20,13 +20,13 @@ public class AssistantHome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assistant_home);
 
-//        arrowback = findViewById(R.id.arrowback);
-//        arrowback.setOnClickListener(view -> {
-//            Intent intent = new Intent(AssistantHome.this,Staff.class);
-//            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
-//            startActivity(intent);
-//            finish();
-//        });
+        arrowback = findViewById(R.id.arrowback);
+        arrowback.setOnClickListener(view -> {
+            Intent intent = new Intent(AssistantHome.this,Staff.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+            finish();
+        });
 
         floatingActionButton = findViewById(R.id.floatingActionButton);
         floatingActionButton.setOnClickListener(view -> {
